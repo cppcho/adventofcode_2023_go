5fcce991f7c4990e78ad93d4cb7e34afa262dae5
cppcho <cppcho.hk@gmail.com>
2023-12-17 00:51:47 +0900

    Solve day9 part2

--- day9/day9.go
+++ day9/day9.go
@@ -10,10 +10,10 @@ import (
 //go:embed input.txt
 var input string
 
-func SolvePart1() {
+func parse() [][]int {
 	lines := strings.Split(input, "\n")
-	output := 0
-	for _, line := range lines {
+	parsed := make([][]int, len(lines))
+	for i, line := range lines {
 		nums := make([]int, 0)
 		for _, str := range strings.Split(line, " ") {
 			num, err := strconv.Atoi(str)
@@ -22,6 +22,15 @@ func SolvePart1() {
 			}
 			nums = append(nums, num)
 		}
+		parsed[i] = nums
+	}
+	return parsed
+}
+
+func SolvePart1() {
+	parsed := parse()
+	output := 0
+	for _, nums := range parsed {
 		for i := 0; i < len(nums); i++ {
 			allZeroes := true
 			for j := 1; j < len(nums)-i; j++ {
@@ -40,10 +49,30 @@ func SolvePart1() {
 		}
 		output += sum
 	}
-
 	fmt.Printf("Part1: %d\n", output)
 }
 
 func SolvePart2() {
-
+	parsed := parse()
+	output := 0
+	for _, nums := range parsed {
+		for i := 0; i < len(nums); i++ {
+			allZeroes := true
+			for j := len(nums) - 1; j > i; j-- {
+				nums[j] = nums[j] - nums[j-1]
+				if nums[j] != 0 {
+					allZeroes = false
+				}
+			}
+			if allZeroes {
+				break
+			}
+		}
+		curr := 0
+		for i := len(nums) - 1; i >= 0; i-- {
+			curr = nums[i] - curr
+		}
+		output += curr
+	}
+	fmt.Printf("Part2: %d\n", output)
 }
